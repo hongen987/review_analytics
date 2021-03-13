22a5c27095a718150a532d30449d12f61264e247
Allen <0912361632allen@gmail.com>
2021-03-14 02:48:37 +0800

    final_virsion

--- review.py
+++ review.py
@@ -2,7 +2,7 @@ data = []
 count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
-        data.append(line.strip())
+        data.append(line)
         count += 1#=count = count + 1
         if count % 100000 == 0:#{%}用來求餘數
             print(count)
@@ -11,3 +11,9 @@ x = 0
 for lines in data:
         x += len(lines)
 print(x/len(data))
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print(len(new))
+print(new[1])
