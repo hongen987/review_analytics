8b31260e941157091fcd2c1d9976725886b94d77
Allen <0912361632allen@gmail.com>
2021-03-13 23:42:27 +0800

    calculate_ave_len_of_reviews

--- review.py
+++ review.py
@@ -1,10 +1,13 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line.strip())
-		count += 1#=count = count + 1
-		if count % 100000 == 0:#{%}用來求餘數
-		    print(count)
-print(len(data))
-print(data[0])#印出第0筆資料
+    for line in f:
+        data.append(line.strip())
+        count += 1#=count = count + 1
+        if count % 100000 == 0:#{%}用來求餘數
+            print(count)
+print('已讀取完檔案, 總共有', len(data), '筆資料')
+x = 0
+for lines in data:
+        x += len(lines)
+print(x/len(data))
